e1b1876eb7df5246c2fbb76dc010f7d5b5a528a7
Guy Mathieu <57466994+GuyEMathieu@users.noreply.github.com>
2022-01-11 13:49:14 -0500

    created shared folder

---
+++ client/src/context/shared/customers.js
@@ -0,0 +1,82 @@
+export const CUSTOMERS = [
+    {
+        "_id": "6146a1fe8f352603dd39f83d",
+        "firstName": "Albert",
+        "lastName": "Gonzalez",
+        "dateOfBirth": "1965-09-15T04:00:00.000Z",
+        "email": "Albert.gonzalez@mail.com",
+        "gender": "611aee3461093e018146791e",
+        "phone": "555-444-3322",
+        "address": {
+            "aptNum": "15B",
+            "_id": "6146a1fe8f352603dd39f83e",
+            "street": "123 Main street",
+            "city": "Mid Town",
+            "state": "610fe766b3fd433be84d0d67",
+            "country": "610fe75fb3fd433be84d0d26",
+            "zipcode": "33312"
+        },
+        "vehicles": [
+            {
+                "mileage": 12035,
+                "_id": "6146a1fe8f352603dd39f83f",
+                "year": 2020,
+                "make": "610ede7a7f39e64068610838",
+                "model": "610ede967f39e640686108b9",
+                "vin": "MK4F5V6EZYUB0NTWN",
+                "serviceLogs": [],
+                "lastModified": "2021-09-19T02:35:42.185Z"
+            },
+            {
+                "mileage": 5,
+                "_id": "6146aa4a4de2e3041836034e",
+                "year": 2015,
+                "make": "610ede7a7f39e6406861083a",
+                "model": "610ede967f39e640686108ca",
+                "vin": "1JX4J7AT25SY92J3S",
+                "serviceLogs": [],
+                "lastModified": "2021-09-19T03:11:06.686Z"
+            },
+            {
+                "mileage": 45,
+                "_id": "61487bf437f87b04d65a9c73",
+                "year": 2020,
+                "make": "610ede7a7f39e6406861084f",
+                "model": "610ede967f39e640686108ec",
+                "vin": "SLEQXTTR8XSXKUMB3",
+                "serviceLogs": [],
+                "lastModified": "2021-09-20T12:17:56.383Z"
+            },
+            {
+                "mileage": 45,
+                "_id": "61487d0737f87b04d65a9c92",
+                "year": 2018,
+                "make": "610ede7a7f39e64068610843",
+                "model": "610ede967f39e640686108c9",
+                "vin": "P0LR8Y6TDWPLYK3VM",
+                "serviceLogs": [],
+                "lastModified": "2021-09-20T12:22:31.166Z"
+            },
+            {
+                "mileage": 67,
+                "_id": "61487d7037f87b04d65a9cb1",
+                "year": 2015,
+                "make": "610ede7a7f39e6406861084d",
+                "model": "610ede967f39e640686108e7",
+                "vin": "NJJ8L8THZN1RDE4HJ",
+                "serviceLogs": [],
+                "lastModified": "2021-09-20T12:24:16.042Z"
+            },
+            {
+                "mileage": 67,
+                "_id": "61487d9237f87b04d65a9cc4",
+                "year": 2019,
+                "make": "610ede7a7f39e64068610857",
+                "model": "610ede967f39e6406861090c",
+                "vin": "1SJ999RKEFZ8XF1WX",
+                "serviceLogs": [],
+                "lastModified": "2021-09-20T12:24:50.348Z"
+            }
+        ]
+    }
+]
