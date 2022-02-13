d3401a2b39172f7445fe577e2d998d01f6bc0cfd
Guy Mathieu <57466994+GuyEMathieu@users.noreply.github.com>
2022-02-13 18:44:33 -0500

    make it better

--- server.js
+++ server.js
@@ -66,5 +66,14 @@ app.use('/api/employees', require('./routes/employeeRoutes/employeeController'))
 app.use('/api/services', require('./routes/serviceRoutes/serviceController'))
 
 
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "index.html"))
+    })
+}
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`SERVER STARTED ON PORT ${PORT}`.cyan.underline.bold))
