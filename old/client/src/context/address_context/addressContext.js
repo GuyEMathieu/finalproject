74708865e4a86ead278fed2fad4d044d3d4ff6ab
Guy Mathieu <57466994+GuyEMathieu@users.noreply.github.com>
2022-02-20 05:29:00 -0500

    removed older files

--- old/client/src/context/address_context/addressContext.js
+++
@@ -1,6 +0,0 @@
-export const ADD_COUNTRY = 'ADD_COUNTRY';
-export const ADD_STATE = 'ADD_STATE';
-export const SET_ERROR = 'SET_ERROR';
-export const REMOVE_ERROR = 'REMOVE_ERROR'
-export const GET_STATES = 'GET_STATES'
-export const GET_COUNTRIES = 'GET_COUNTRIES'
--- old/client/src/context/auth_context/authTypes.js
+++
@@ -1,8 +0,0 @@
-export const CLEAR_ERROR = 'CLEAR_ERROR'
-export const SET_ERRORS = 'SET_ERRORS'
-export const AUTH_ERROR = 'AUTH_ERROR'
-
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
-export const LOGIN_FAIL = 'LOGIN_FAIL'
-export const USER_LOADED = 'USER_LOADED'
-export const USER_LOGOUT = 'USER_LOGOUT'
